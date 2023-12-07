feeb21ca715c4518cab8bfbe1bfc032f1cd80016
Daniel Flores <daniel.flores.ang@gmail.com>
2023-12-06 20:24:52 -0600

    agregando styles

--- script/main.js
+++ script/main.js
@@ -1,12 +1,16 @@
 
 
-// Input
+// Cantidad ingresada por el usuario
 let ingresarCantidad = document.getElementById("ingresarCantidad")
 
-//botón
 let botonActualizarInversion = document.getElementById("botonActualizarInversion")
 
+// inversión actualizada 
 let inversionActual = document.getElementById("inversionActual")
+
+//Historial de depósitos
+let contenedorHistorial = document.getElementById("contenedorHistorial")
+let historialDepositos = []
 
 //valores en el STORAGE
 window.onload = function() {
@@ -24,14 +28,28 @@ botonActualizarInversion.addEventListener("click", function () {
   let cantidadIngresada = ingresarCantidad.value
 
   let historial = {
-    cantidad: cantidadIngresada,
-    fecha: new Date().toLocaleString
+    cantidad: parseInt(cantidadIngresada),
+    fecha: new Date().toLocaleString()
   }
-  inversionActual.innerHTML = historial.cantidad 
 
-  localStorage.setItem("cantidadSumada", JSON.stringify(historial))
+  historialDepositos.push(historial)
+
+  inversionActual.innerHTML = parseInt(historial.cantidad) 
+  // contenedorHistorial.innerHTML = `Cantidad: $${historial.cantidad} - Fecha: ${historial.fecha}`
+
+  localStorage.setItem("cantidadSumada", JSON.stringify(historialDepositos))
 
   ingresarCantidad.value = ""
+  muestraHistorial()
 }
 )
-// console.log(botonActualizarInversion)
+
+
+
+let muestraHistorial = () => {
+  historialDepositos.forEach(function(cantidad) 
+  {
+    contenedorHistorial.innerHTML += `Cantidad: $${cantidad.cantidad} - Fecha: ${cantidad.fecha} <br>`
+  })
+}
+
